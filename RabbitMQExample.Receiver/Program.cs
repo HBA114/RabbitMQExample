83e4ffc837f016302ea13c6df9fbd9a4c5826b5a
HBA114 <hbasriayhaner114@gmail.com>
2023-07-14 21:34:33 +0300

    first commit

---
+++ RabbitMQExample.Receiver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "message-queue", durable: true, exclusive: false, autoDelete: false);
+
+Console.WriteLine("Waiting for messages...");
+
+var consumer = new EventingBasicConsumer(model: channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received Message: {message}");
+};
+
+channel.BasicConsume(queue: "message-queue", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Press Enter to Exit!");
+Console.ReadLine();
---
+++ RabbitMQExample.Sender/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "message-queue", durable: true, exclusive: false, autoDelete: false);
+
+var message = "VBT RabbitMQ Example";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: String.Empty, routingKey: "message-queue", basicProperties: null, body: body);
+
+Console.WriteLine("Message sent to queue!");
